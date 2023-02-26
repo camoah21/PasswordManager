ec8d7cb1ed36804bd7cd8dbaab98bd2307ead673
James Jensen <jjense14@uncc.edu>
2023-02-26 17:00:48 -0500

    Added Edit Button to Password List

--- src/PasswordListFrame.java
+++ src/PasswordListFrame.java
@@ -20,11 +20,33 @@ public class PasswordListFrame extends JFrame {
         this.setTitle("Password List");
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        passwordTableModel = new DefaultTableModel(new String[] {"Website URL", "Website Name", "Username", "Password", "Delete"}, 0);
+        passwordTableModel = new DefaultTableModel(
+                new String[] { "Website URL", "Website Name", "Username", "Password", "Edit", "Delete" }, 0);
         passwordTable = new JTable(passwordTableModel);
         passwordTable.setBounds(10, 10, 780, 450);
-        
-        //Add delete button to each row
+
+        // Add Edit button to each row
+        passwordTable.getColumn("Edit").setCellRenderer(new ButtonRenderer());
+        passwordTable.getColumn("Edit").setCellEditor(new ButtonEditor(new JCheckBox()));
+        passwordTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int row = passwordTable.getSelectedRow();
+                int col = passwordTable.getSelectedColumn();
+                if (col == passwordTable.getColumn("Edit").getModelIndex()) {
+                    int passwordID = (int) passwordTableModel.getValueAt(row, 4);
+                    String URL = (String) passwordTableModel.getValueAt(row, 0);
+                    String website = (String) passwordTableModel.getValueAt(row, 1);
+                    String username = (String) passwordTableModel.getValueAt(row, 2);
+                    String password = (String) passwordTableModel.getValueAt(row, 3);
+                    PasswordEditFrame frame = new PasswordEditFrame(URL, website, username, password);
+                    frame.setVisible(true);
+
+                }
+            }
+        });
+
+        // Add delete button to each row
         passwordTable.getColumn("Delete").setCellRenderer(new ButtonRenderer());
         passwordTable.getColumn("Delete").setCellEditor(new ButtonEditor(new JCheckBox()));
         passwordTable.addMouseListener(new MouseAdapter() {
@@ -33,7 +55,7 @@ public class PasswordListFrame extends JFrame {
                 int row = passwordTable.getSelectedRow();
                 int col = passwordTable.getSelectedColumn();
                 if (col == passwordTable.getColumn("Delete").getModelIndex()) {
-                    int passwordID = (int)passwordTableModel.getValueAt(row, 4);
+                    int passwordID = (int) passwordTableModel.getValueAt(row, 4);
                     sqlIntegration.deletePassword(passwordID);
                     passwordTableModel.removeRow(row);
                 }
@@ -47,21 +69,11 @@ public class PasswordListFrame extends JFrame {
         String webUName = new String();
         String webUPassword = new String();
 
-
         for (String password : passwords) {
-            passwordTableModel.addRow(new String[] {webURL, webName, webUName, webUPassword, password});
+            passwordTableModel.addRow(new String[] { webURL, webName, webUName, webUPassword, password });
         }
 
         this.setVisible(true);
     }
 
 }
-
-
-
-    
-
-
-
-
-
